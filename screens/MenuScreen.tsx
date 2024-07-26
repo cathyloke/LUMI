fd9af03b2d63f08e24a5f3c696dc5b61caee553b
cathyloke <catloke963@gmail.com>
2024-07-26 23:31:07 +0800

    Added Hui Hui version 1

---
+++ screens/MenuScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from '../modules/style';
+
+const MenuScreen = () => {
+   return (
+      <View>
+         <View>
+            <Text style={styles.font}>Menu Screen</Text>
+         </View>
+      </View>
+   );
+};
+
+export default MenuScreen;
