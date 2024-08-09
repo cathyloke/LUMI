6bf0f059f7a9b4c6bfdb4f9a88374148776e9ff6
joelytin <joelytin@outlook.com>
2024-08-09 21:37:15 +0800

    Style FeedbackScreen.tsx

--- screens/FeedbackScreen.tsx
+++ screens/FeedbackScreen.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { styles } from '../modules/homeStyle';
+import { styles } from '../modules/generalStyle';
 
 const FeedbackScreen = () => {
    return (
       <View>
          <View>
-            <Text style={styles.font}>Feedback</Text>
+            <Text style={styles.header}>Feedback</Text>
          </View>
       </View>
    );
