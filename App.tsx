d478b6072604293fb8f460d0d08007a49f82cc09
joelytin <joelytin@outlook.com>
2024-08-09 18:06:09 +0800

    Increase size of bottom tab, add icons

--- App.tsx
+++ App.tsx
@@ -1,23 +1,78 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Dimensions } from 'react-native';
 
 import HomeScreen from './screens/HomeScreen';
 import MenuScreen from './screens/MenuScreen';
 import RewardsScreen from './screens/RewardsScreen';
 import ProfileScreen from './screens/ProfileScreen';
 
+import Feather from 'react-native-vector-icons/Feather';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import Octicons from 'react-native-vector-icons/Octicons';
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
+  
+  const windowHeight = Dimensions.get('window').height;
+  
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Menu" component={MenuScreen} />
-        <Tab.Screen name="Rewards" component={RewardsScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: '#f5f5f5',
+            height: windowHeight * .15,
+          },
+          tabBarActiveTintColor: '#000',
+          tabBarInactiveTintColor: '#999',
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: 'bold',
+            fontFamily: 'Gantari-Regular',
+          },
+        }}
+      >
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{
+            tabBarIcon: (({ focused }: any) => (
+              <Feather name="home" size={35} color={focused ? '#000' : '#999'} />
+            ))
+          }}  
+        />
+        <Tab.Screen 
+          name="Menu" 
+          component={MenuScreen}
+          options={{
+            tabBarIcon: (({ focused }: any) => (
+              <MaterialIcons name="restaurant-menu" size={35} color={focused ? '#000' : '#999'} />
+            ))
+          }}
+        />
+        <Tab.Screen 
+          name="Rewards" 
+          component={RewardsScreen}
+          options={{
+            tabBarIcon: (({ focused }: any) => (
+              <MaterialCommunityIcons name="ticket-outline" size={35} color={focused ? '#000' : '#999'} />
+            ))
+          }}
+        />
+        <Tab.Screen 
+          name="Profile" 
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: (({ focused }: any) => (
+              <Octicons name="person" size={35} color={focused ? '#000' : '#999'} />
+            ))
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
