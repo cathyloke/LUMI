0ffe9f92347c6fe2e846ffead8266887b07b9b78
Zoriannn <jzchoo2004@gmail.com>
2024-08-14 01:09:36 +0800

    added feedback section

--- modules/profileStyle.ts
+++ modules/profileStyle.ts
@@ -81,4 +81,73 @@ export const styles = StyleSheet.create({
     fontSize: 18,
     color: '#3E2723', // Dark Chocolate
   },
+
+  title:{
+    fontFamily: 'Gantari-Regular',
+    fontSize: 28,
+    textAlign: 'center',
+    color: '#102C57',
+    marginTop: 20,
+    fontWeight: 'bold'
+  },
+
+  subtitle:{
+    marginTop:10,
+    marginBottom:30,
+    color:'#000000',
+    textAlign:'center'
+  },
+
+  box: {
+    width: 100,
+    height: 50,
+    backgroundColor: '#ddd',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 4,
+    borderRadius: 20,
+  },
+
+  selectedBox: {
+      backgroundColor: '#102C57',
+  },
+
+  text: {
+      color: '#000',
+  },
+
+  selectedText: {
+    color: '#F8F0E5',
+ },
+
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginVertical: 3,
+  },
+
+  comments:{
+    marginTop:20,
+    color:'#000000',
+    textAlign: 'left',
+    marginLeft:20,
+  },
+
+  inputs:{
+    height: 150,
+    margin: 14,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+
+  subbutton: {
+    backgroundColor: '#102C57',  
+    paddingVertical: 10,
+    paddingHorizontal: 30,  
+    borderRadius: 20, 
+    alignItems: 'center',
+    alignSelf: 'center', 
+    width: 200,
+  },
 });
