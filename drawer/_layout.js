7ee2ca05b65e2aa234fb0a9f1630be9814965a9c
joelytin <joelytin@outlook.com>
2024-08-19 10:41:32 +0800

    Delete drawer folder

--- drawer/_layout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-
-export default function Layout() {
-   return (
-
-   )
-}
