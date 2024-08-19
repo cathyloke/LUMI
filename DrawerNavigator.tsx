f739d4af6f64bdbcc7fb411174534e954b73ae6f
joelytin <joelytin@outlook.com>
2024-08-19 21:38:09 +0800

    Add DrawerNavigator.tsx

---
+++ DrawerNavigator.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './screens/HomeScreen';
+import MenuScreen from './screens/MenuScreen';
+import RewardsScreen from './screens/RewardsScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import CustomDrawer from './components/CustomDrawer'; // Custom drawer content
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawer {...props} />}  
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Menu" component={MenuScreen} />
+      <Drawer.Screen name="Rewards" component={RewardsScreen} />
+      <Drawer.Screen name="Profile" component={ProfileScreen} />
+    </Drawer.Navigator>
+  );
+};
+
+export default DrawerNavigator;
