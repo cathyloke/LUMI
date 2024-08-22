7d9f8a09c00d02628acc14a20dcf722b70f65eaf
cathyloke <catloke963@gmail.com>
2024-08-22 11:23:51 +0800

    Updated the database and MenuScreen

---
+++ assets/dbConnection.ts
@@ -0,0 +1,109 @@
+import {SQLiteDatabase, enablePromise, openDatabase} from 'react-native-sqlite-storage';
+
+const databaseName = 'lumiere.sqlite';
+
+enablePromise(true);
+
+export const getDBConnection = async() => {
+    return await openDatabase(
+        {name: `${databaseName}`, 
+        createFromLocation:`~${databaseName}`},
+      openCallback,
+      errorCallback,
+    );
+}
+
+export const createTableUsers = async( db: SQLiteDatabase ) => {
+    try{
+        const query = 'CREATE TABLE IF NOT EXISTS users(id VARCHAR(10) PRIMARY KEY, name VARCHAR(20), phone VARCHAR(20), password VARCHAR(20))';
+        await db.executeSql(query);
+      } catch (error) {
+        console.error(error);
+        throw Error('Failed to create table !!!');
+      }
+}
+
+export const createTableMenu = async( db: SQLiteDatabase ) => {
+    try{
+        const query = 'CREATE TABLE IF NOT EXISTS menu(id VARCHAR(10) PRIMARY KEY, name VARCHAR(20), category VARCHAR(20), type VARCHAR(10), description VARCHAR2(100), image VARCHAR2(50), price NUMERIC(10,2))';
+        await db.executeSql(query);
+      } catch (error) {
+        console.error(error);
+        throw Error('Failed to create table !!!');
+      }
+}
+
+export const createTableOrderHistory = async( db: SQLiteDatabase ) => {
+    try{
+        const query = 'CREATE TABLE IF NOT EXISTS orderHistory(id VARCHAR(10) PRIMARY KEY AUTOINCREMENT, userID VARCHAR(20), foodID VARCHAR(20), date VARCHAR(12), quantity VARCHAR(10))';
+        await db.executeSql(query);
+      } catch (error) {
+        console.error(error);
+        throw Error('Failed to create table !!!');
+      }
+}
+
+
+export const getUsers = async( db: SQLiteDatabase ): Promise<any> => {
+    try{
+        const userData : any = [];
+        const query = `SELECT * FROM users`;
+        const results = await db.executeSql(query);
+        results.forEach(result => {
+            (result.rows.raw()).forEach(( item:any ) => {
+                userData.push(item);
+            })
+          });
+        return userData;
+      } catch (error) {
+        console.error(error);
+        throw Error('Failed to get users !!!');
+      }
+}
+
+export const getMenuData = async (db: SQLiteDatabase, category?: string): Promise<any> => {
+  try {
+    const menuData: any = [];
+    const query = category ? `SELECT * FROM menu WHERE category=?` : `SELECT * FROM menu`;
+    const params = category ? [category] : [];
+    
+    const results = await db.executeSql(query, params);
+    results.forEach(result => {
+      (result.rows.raw()).forEach((item: any) => {
+        menuData.push(item);
+      });
+    });
+    
+    return menuData;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to get Menu Data!');
+  }
+};
+
+
+export const getOrderHistory = async( db: SQLiteDatabase ): Promise<any> => {
+    try{
+        const orderHistoryData : any = [];
+        const query = `SELECT * FROM orderHistory`;
+        const results = await db.executeSql(query);
+        results.forEach(result => {
+            (result.rows.raw()).forEach(( item:any ) => {
+                orderHistoryData.push(item);
+            })
+          });
+        return orderHistoryData;
+      } catch (error) {
+        console.error(error);
+        throw Error('Failed to get order history !!!');
+      }
+}
+
+
+const openCallback = () => {
+    console.log('database open success');
+}
+
+const errorCallback = (err: any) => {
+    console.log('Error in opening the database: ' + err);
+}
--- screens/MenuScreen.tsx
+++ screens/MenuScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Image, FlatList, TouchableOpacity, SafeAreaView,TouchableHighlight, } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { createStackNavigator} from '@react-navigation/stack';
@@ -6,146 +6,26 @@ import { styles } from '../modules/menuStyle';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Entypo from 'react-native-vector-icons/Entypo';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
-const Stack=createStackNavigator();
+import {getDBConnection, getMenuData } from "../assets/dbConnection";
 
 type FoodItem={
   id: string;
-  title: string;
-  price: string;
-  image: any;
+  name: string;
+  category: string;
+  type: any;
   description: string;
-  type: string;
+  image: string;
+  price: number;
 };
 
 type Categories={
   [key:string]:FoodItem[];
 };
 
-const menu:Categories={ //item
-  'Food':[
-  {
-    id:'001',
-    title:'Egg croissant sandwich',
-    price:'RM 8.00',
-    image:require('../img/food/FoodEggCroissantSandwich.png'),
-    description:'A delicious Egg Croissant Sandwich is suit for you as a breakfast',
-    type:'Breakfast'
-  },
-  {
-    id:'002',
-    title:'Berry Pancakes',
-    price:'RM 9.90',
-    image:require('../img/food/FoodBerryPancakes.png'),
-    description:'Berry Pancakes, a sweet and sour taste sure will make you happy whole day',
-    type:'Dessert'
-  },
-  {
-    id:'003',
-    title:'Avocado toast',
-    price:'RM 8.90',
-    image:require('../img/food/FoodAvocadoToast.png'),
-    description:'Avocado Toast, fast prepared breakfast, speed up and save your time',
-    type:'Breakfast'
-  },{
-    id:'004',
-    title:'Chocolate cake',
-    price:'RM 15.90',
-    image:require('../img/food/FoodChocolateCake.png'),
-    description:'Chocolate Cake, a cake flavored with melted chocolate,and cocoa powder put on top',
-    type:'Dessert'
-  },{
-    id:'005',
-    title:'New York cheesecake',
-    price:'RM 12.90',
-    image:require('../img/food/FoodNewYorkCheesecake.jpeg'),
-    description:'New York Cheesecake, full of cheese taste and smooth texture',
-    type:'Dessert'
-  },{
-    id:'006',
-    title:'Tiramisu cake',
-    price:'RM 13.90',
-    image:require('../img/food/FoodTiramisuCake.jpg'),
-    description:'Tiramisu Cake, a vanilla sponge cakes soaked in coffee, frosted with a fluffy mascarpone cream and topped with a dusting of cocoa powder',
-    type:'Dessert'
-  },{
-    id:'007',
-    title:'Hokkaido Cheese Tart',
-    price:'RM 6.90',
-    image:require('../img/food/FoodHokkaidoCheeseTart.png'),
-    description:'Hokkaido Cheese Tart, delicious cheese tart with full of cheese',
-    type:'Dessert'
-  },{
-    id:'008',
-    title:'Macarons',
-    price:'RM 4.90',
-    image:require('../img/food/FoodMacarons.jpg'),
-    description:'Macarons, a sweet meringue-based confection dessert',
-    type:'Dessert'
-  }],
-  'Drink':[
-  {
-    id:'001',
-    title:'Americano',
-    price:'RM 5.90',
-    image:require('../img/drinks/DrinksAmericano.png'),
-    description:'Americano, diluting an espresso shot with hot water, have a lighter taste',
-    type:'Coffee'
-  },{
-    id:'002',
-    title:'Latte',
-    price:'RM 9.90',
-    image:require('../img/drinks/DrinksLatte.png'),
-    description:'Latte, made by espresso and steamed milk',
-    type:'Coffee'
-  },{
-    id:'003',
-    title:'Latte special',
-    price:'RM 12.90',
-    image:require('../img/drinks/DrinksLatteSpecial.png'),
-    description:'Latte, made by espresso and steamed milk, added with chocolate and ice cream',
-    type:'Coffee'
-  },{
-    id:'004',
-    title:'Cappuccino',
-    price:'RM 8.90',
-    image:require('../img/drinks/DrinksCappuccino.png'),
-    description:'Cappuccino, espresso-based coffee drink with steamed milk including a layer of milk foam',
-    type:'Coffee'
-  },{
-    id:'005',
-    title:'Mocha',
-    price:'RM 9.90',
-    image:require('../img/drinks/DrinksMocha.png'),
-    description:'Mocha, used good quality coffee that is made from a specific coffee bean',
-    type:'Coffee'
-  },{
-    id:'006',
-    title:'Matcha Latte',
-    price:'RM 12.90',
-    image:require('../img/drinks/DrinksMatchaLatte.jpeg'),
-    description:'Matcha Latte, Latte with mixing of matcha powder, steaming hot milk and honey',
-    type:'Coffee'
-  },{
-    id:'007',
-    title:'Matcha Milk Tea',
-    price:'RM 12.90',
-    image:require('../img/drinks/DrinksMatchaMilkTea.png'),
-    description:'Matcha Milk Tea, mixing of matcha powder and milk, added with boba',
-    type:'Milk Tea'
-  },{
-    id:'008',
-    title:'Taro Milk Tea',
-    price:'RM 10.90',
-    image:require('../img/drinks/DrinksTaroMilkTea.png'),
-    description:'Taro Milk Tea, A drink that popular in Taro Lovers',
-    type:'Milk Tea'
-  }]
-}
-
+const Stack=createStackNavigator();
 
 const CustomStackContent = ({ navigation }: any) => { //side bar
-  const [activeCategory, setActiveCategory] = useState<string>('Food');
+  const [activeCategory, setActiveCategory] = useState<string>('All');
 
   const handlePress = (category: string) => {
     setActiveCategory(category);
@@ -154,6 +34,17 @@ const CustomStackContent = ({ navigation }: any) => { //side bar
 
   return (
     <View style={styles.sidebar}>
+      <TouchableHighlight
+        style={[styles.sidebarItem, activeCategory === 'All' && { backgroundColor: '#DAC0A3' }]}
+        underlayColor="#DAC0A3"
+        onPress={() => handlePress('All')}
+      >
+        <View style={{ alignItems: 'center' }}>
+          <MaterialCommunityIcons name="filter" size={24} color={activeCategory === 'All' ? '#102C57' : '#333'} />
+          <Text style={[styles.sidebarText, activeCategory === 'All' && { color: '#102C57' }]}>All</Text>
+        </View>
+      </TouchableHighlight>
+
       <TouchableHighlight
         style={[styles.sidebarItem, activeCategory === 'Food' && { backgroundColor: '#DAC0A3' }]}
         underlayColor="#DAC0A3"
@@ -210,22 +101,58 @@ const ItemDetailScreen = ({route}: any) => { //item detail screen
 
 
 const CategoryScreen = ({navigation, route}: any) => {
-  const {category}: {category?: keyof Categories}=route.params||{};
 
-  if (!category||!menu[category]) {
+  const [foodMenu, setFoodMenu] = useState<FoodItem[]>([]);
+  const [drinkMenu, setDrinkMenu] = useState<FoodItem[]>([]);
+  const [menu, setMenu] = useState<Categories>({});
+  const [selectedType, setSelectedType] = useState<string>('All');
+  
+  const query = async () => {
+    try {
+      const db = await getDBConnection();
+      const foodMenuData = await getMenuData(db, 'Food');
+      const drinkMenuData = await getMenuData(db, 'Drink');
+
+      setFoodMenu(foodMenuData);
+      setDrinkMenu(drinkMenuData);
+
+      setMenu({
+        Food: foodMenuData,
+        Drink: drinkMenuData
+      });
+    } catch (error) {
+      console.error("Error fetching menu data:", error);
+    }
+  };
+  
+
+  useEffect(() => {
+    query();
+  }, [])
+
+  const {category} =route.params||{};
+  const currentMenu = menu[category] || [];
+
+  if (!category) {
     return (
       <SafeAreaView style={styles.container}>
         <Text>Category not found</Text>
       </SafeAreaView>
     );
   }
+  if (!currentMenu.length) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>Menu not found</Text>
+      </SafeAreaView>
+    );
+  }
 
-  const [selectedType, setSelectedType] = useState<string>('All');
+  const foodTypes = ['All', ...new Set(currentMenu.map(item=>item.type))];
 
-  const foodTypes = ['All', ...new Set(menu[category].map(item=>item.type))];
+  const filteredItems = selectedType === 'All' ? currentMenu:currentMenu.filter(item=>item.type === selectedType);
 
-  const filteredItems = selectedType === 'All' ? menu[category]:menu[category].filter(item=>item.type === selectedType);
-
+ 
   return (
     <SafeAreaView style={styles.container}>
       <SafeAreaView style={styles.content}>
@@ -234,7 +161,9 @@ const CategoryScreen = ({navigation, route}: any) => {
           style={styles.picker} 
           onValueChange={(itemValue) => setSelectedType(itemValue)}
         >
-          {foodTypes.map(type => (<Picker.Item key={type} label={type} value={type}/>))}
+          {foodTypes.map(type => (
+            <Picker.Item key={type} label={type} value={type}/>
+          ))}
         </Picker>
 
         <FlatList
@@ -246,10 +175,11 @@ const CategoryScreen = ({navigation, route}: any) => {
               onPress={() => navigation.navigate('ItemDetail', { item })}
             >
               <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Image source={item.image} style={styles.imageRow} />
+                <Image source={{ uri: item.image }} style={styles.imageRow} />
+                
                 <View style={styles.textContainer}>
-                  <Text style={styles.title}>{item.title}</Text>
-                  <Text style={styles.price}>{item.price}</Text>
+                  <Text style={styles.title}>{item.name}</Text>
+                  <Text style={styles.price}>RM{item.price}</Text>
                 </View>
               </View>
             </TouchableHighlight>
